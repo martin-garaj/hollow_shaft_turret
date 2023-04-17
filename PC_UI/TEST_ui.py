64ac03c1ca6c301637ecfca6d92627981182a11f
Martin Garaj <martin.garaj@kinit.sk>
2023-04-17 18:05:25 +0200

    Updating UI on PC side

--- PC_UI/TEST_ui.py
+++ PC_UI/TEST_ui.py
@@ -3,6 +3,7 @@ import serial
 from PyQt5.QtCore import QThread, QObject, pyqtSignal
 from PyQt5.QtWidgets import QApplication, QWidget, QTextEdit, QVBoxLayout, QHBoxLayout, QRadioButton, QPushButton
 from PyQt5 import QtGui
+import binascii
 
 from pfm_config import *
 from pkt_cmd_defs import *
@@ -49,9 +50,21 @@ def decode_message(data):
     return payload, remaining_data
 
 
+def find_start_bytes(byte_array, start_bytes):
+    for i in range(len(byte_array)-1):
+        if byte_array[i:i+2] == start_bytes:
+            return i
+    return -1  # start bytes not found
 
+def decode_packet_type(byte_array):
+    command_type = byte_array[1]
+    return command_type
 
-
+# def decode_imu_packet(byte_array):
+#     payload_size = int(byte_array[0])-1
+#     payload = byte_array[2:2+payload_size]
+#     imu_data = {''}
+#     return 
 
 
 class SerialReader(QObject):
@@ -60,24 +73,76 @@ class SerialReader(QObject):
     def __init__(self, port, baudrate):
         super().__init__()
         self.serial = serial.Serial(port, baudrate)
+        self.raw_data = bytearray()
 
     def run(self):
         while True:
             if self.serial.inWaiting() > 0:
-                data = self.serial.read(self.serial.in_waiting).decode('utf-8', errors='backslashreplace')
-                self.data_ready.emit(data)
-            # if self.serial.inWaiting() > 0:
-            #     # data = self.serial.read(self.serial.in_waiting).decode('utf-8', errors='backslashreplace')
-            #     data += str(self.serial.read(self.serial.in_waiting).decode('utf-8', errors='backslashreplace'))
+                ### This works (only for testing)
+                # print('.', end="", flush=True)
+                # data = self.serial.read(self.serial.in_waiting).decode('utf-8', errors='backslashreplace')
+                # self.data_ready.emit('EMIT: '+data)
                 
-            #     # print(type(data))
-            #     console_oupput, _ = decode_message(data)
-            #     if not isinstance(console_oupput, type(None)):
-            #         self.data_ready.emit(console_oupput)
-            #         # self.data_ready.emit(console_oupput)
-            #     else:
-            #         self.data_ready.emit(data)
+                ### This works (for decoding and better readability of messages)
+                # print(data, flush=True)
+                # data = self.serial.read(self.serial.in_waiting)
+                # hex_data = binascii.hexlify(data).decode('ascii')
+                # hex_list = [hex_data[i:i+2] for i in range(0, len(hex_data), 2)]
+                # hex_str = ":" + "-".join(hex_list) + "\n"     
+                # self.data_ready.emit(hex_str)       
                 
+                ### This DOESNT WORK (for catching the beginning and end of the string)
+                # # accumulate data until the end of packet is detected
+                # data = bytearray(self.serial.read(self.serial.in_waiting))
+                
+                # self.raw_data.extend(data)
+                # # print(f"len:{len(self.raw_data)}  {self.raw_data.decode('utf-8', errors='backslashreplace')}", flush=True)
+                # if self.raw_data[-2:] == END_BYTES:
+                #     idx_start_byte = find_start_bytes(self.raw_data, START_BYTES)
+                #     self.data_ready.emit(".") 
+                #     if idx_start_byte > -1:
+                #         # self.data_ready.emit(self.raw_data[idx_start_byte+2:-2].decode())
+                        
+                #         hex_data = binascii.hexlify(self.raw_data[idx_start_byte+2:-2]).decode('ascii')
+                #         hex_list = [hex_data[i:i+2] for i in range(0, len(hex_data), 2)]
+                #         hex_str = ":" + "-".join(hex_list) + "\n"     
+                #         self.data_ready.emit(hex_str)
+                        
+                #     else:
+                #         self.data_ready.emit("PACKET LOST")
+                # else:
+                #     print(self.raw_data[-2:].decode('utf-8', errors='backslashreplace'))
+                #     self.data_ready.emit(self.raw_data[-2:].decode('utf-8', errors='backslashreplace'))
+                    
+                # Read the data and append it to the raw_data buffer
+                data = bytearray(self.serial.read(self.serial.in_waiting))
+                self.raw_data.extend(data)
+
+                # Search for the END_BYTES in the raw_data buffer
+                end_index = self.raw_data.find(END_BYTES)
+                while end_index != -1:
+                    # Check if there are any START_BYTES before the END_BYTES
+                    start_index = self.raw_data.rfind(START_BYTES, 0, end_index)
+                    if start_index != -1:
+                        # Extract the payload and emit it as hex string
+                        payload = self.raw_data[start_index+2:end_index]
+                        hex_data = binascii.hexlify(payload).decode('ascii')
+                        hex_list = [hex_data[i:i+2] for i in range(0, len(hex_data), 2)]
+                        hex_str = ":" + "-".join(hex_list) + "\n"
+                        
+                        
+                        self.data_ready.emit(hex_str)
+
+                        # Remove the extracted packet from the raw_data buffer
+                        self.raw_data = self.raw_data[end_index+len(END_BYTES):]
+                    else:
+                        pass
+                        # There may be another packet after the current END_BYTES
+                        self.raw_data = self.raw_data[end_index+len(END_BYTES):]
+                    # Search for the next END_BYTES in the remaining raw_data
+                    end_index = self.raw_data.find(END_BYTES)
+                
+
     def write_serial(self, message):
         self.serial.write(message.encode())
 
@@ -145,9 +210,10 @@ class MainWindow(QWidget):
                                 0, # payload: pfm direction
                                 ],
                             )
+            print('DIR: False sent', flush=True)
+            print(message, flush=True)
             message = encode_message(message)
-            print(message)
-            
+
         elif self.radio_btn_2.isChecked():
             message = bytearray([
                                 int.from_bytes(CMD_SET_TARGET_FREQ, byteorder=BYTEORDER), # command
@@ -157,29 +223,33 @@ class MainWindow(QWidget):
                                 255, # payload: pfm direction
                                 ],
                             )
+            print('DIR: True sent', flush=True)
+            print(message, flush=True)
             message = encode_message(message)
-            print(message)
         elif self.radio_btn_3.isChecked():
             message = bytearray([
                                 int.from_bytes(CMD_ENABLE_CNC, byteorder=BYTEORDER), # command
                                 ],
                             )
+            print('ENABLE sent', flush=True)
+            print(message, flush=True)
             message = encode_message(message)
-            print(message)
         elif self.radio_btn_4.isChecked():
             message = bytearray([
                                 int.from_bytes(CMD_DISABLE_CNC, byteorder=BYTEORDER), # command
                                 ],
                             )
             message = encode_message(message)
-            print(message)
+            print('DISABLE sent', flush=True)
+            print(message, flush=True)
         elif self.radio_btn_5.isChecked():
             message = bytearray([
                                 int.from_bytes(CMD_GET_IMU_MEASUREMENT, byteorder=BYTEORDER), # command
                                 ],
                             )
+            print('Sending IMU message', flush=True)
+            print(message, flush=True)
             message = encode_message(message)
-            print(message)
         else:
             message = "No message selected"
             
